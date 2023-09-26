4818a0f253f3a2646e8ba525c3375b5aa7e1ebfb
sachinInfra25 <142871853+sachinInfra25@users.noreply.github.com>
2023-09-26 11:31:06 +0530

    Tech Stack, Testimonials and Blog sections completed

--- New folder/js/infra.js
+++ New folder/js/infra.js
@@ -72,43 +72,139 @@
 })();
 // Text
 
-const tl = gsap.timeline({repeat: -1, repeadDelay: 0});
+// function textAnimaiton(text, bg){
+//   const titleTl = new TimelineMax();
 
-tl.from(".line span", {
-  y: 100,
-  delay: 0,
-	duration: .5,
-  transformOrigin: "0% 50% -50",
-}).to(".line span", {
-  y: -100,
-  ease: "none",
-  transformOrigin: "0% 50% -50",
-  delay: 1.5,
-	duration: .3,
+//   titleTl.from(text, {
+//     ease: "none",
+//     yPercent: 100,
+//     duration: .4,
+//     onUpdate: function(){
+//       let target = this.targets()[0];
+//       document.querySelector("#animated-text").style.height = `${target.clientHeight}px`;
+//     },
+//   }).to(text, {
+//     ease: "none",
+//     yPercent: -100,
+//     duration: .4,
+//     delay: .5,
+//   }).fromTo(bg, {
+//     ease: "none",
+//     duration: .5,
+//     backgroundPosition: "0 0",
+//     height: 0,
+//     bottom: 0,
+//   }, {
+//     ease: "none",
+//     duration: .5,
+//     backgroundPosition: "0 50%",
+//     height: "100%",
+//     top: 0,
+//   }, "-=1.8").to(bg, {
+//     ease: "none",
+//     backgroundPosition: "0 100%",
+//     height: 0,
+//     bottom: 0,
+//   });
+
+//   return titleTl;
+// }
+
+// const masterTl = new TimelineMax()
+// masterTl.add(textAnimaiton("#animated-text .line", "#banner .backgrounds .ai"), "-=.8")
+// .add(textAnimaiton("#animated-text .lineOne", "#banner .backgrounds .ml"), "-=.8")
+// .add(textAnimaiton("#animated-text .lineTwo", "#banner .backgrounds .web3"), "-=.8");
+
+const titleTl = new TimelineMax({repeat: -1});
+
+titleTl.from("#animated-text .line", {
+	yPercent: 100,
+  duration: .4,
+  onUpdate: function(){
+    let target = this.targets()[0];
+    document.querySelector("#animated-text").style.height = `${target.clientHeight}px`;
+  },
+}).to("#animated-text .line", {
+	yPercent: -100,
+	ease: "none",
+  duration: .4,
+	delay: 1,
+}).fromTo("#banner .backgrounds .ai", 1, {
+  backgroundPosition: "0 0",
+  height: 0,
+  bottom: 0,
+}, {
+  backgroundPosition: "0 50%",
+  height: "100%",
+  top: 0,
+}, "ai-=2.8").to("#banner .backgrounds .ai", 1, {
+  backgroundPosition: "0 100%",
+  height: 0,
+  bottom: 0,
+}).from("#animated-text .lineOne", {
+	yPercent: 100,
+  duration: .4,
+  onUpdate: function(){
+    let target = this.targets()[0];
+    document.querySelector("#animated-text").style.height = `${target.clientHeight}px`;
+  },
+}).to("#animated-text .lineOne", {
+	yPercent: -100,
+	ease: "none",
+  duration: .4,
+	delay: 1,
+}).fromTo("#banner .backgrounds .ml", 1, {
+  backgroundPosition: "0 0",
+  height: 0,
+  bottom: 0,
+}, {
+  backgroundPosition: "0 50%",
+  height: "100%",
+  top: 0,
+}, "ml-=2.8").to("#banner .backgrounds .ml", 1, {
+  backgroundPosition: "0 100%",
+  height: 0,
+  bottom: 0,
+}).from("#animated-text .lineTwo", {
+	yPercent: 100,
+  duration: .4,
+  onUpdate: function(){
+    let target = this.targets()[0];
+    document.querySelector("#animated-text").style.height = `${target.clientHeight}px`;
+  },
+}).to("#animated-text .lineTwo", {
+	yPercent: -100,
+	ease: "none",
+  duration: .4,
+	delay: 1,
+}).fromTo("#banner .backgrounds .web3", 1, {
+  backgroundPosition: "0 0",
+  height: 0,
+  bottom: 0,
+}, {
+  backgroundPosition: "0 50%",
+  height: "100%",
+  top: 0,
+}, "web3-=2.8").to("#banner .backgrounds .web3", 1, {
+  backgroundPosition: "0 100%",
+  height: 0,
+  bottom: 0,
 });
-tl.from(".lineOne span", {
-  y: 100,
-  delay: 0,
-	duration: .5,
-  transformOrigin: "0% 50% -50",
-}).to(".lineOne span", {
-  y: -100,
-  ease: "none",
-  transformOrigin: "0% 50% -50",
-  delay: 1.5,
-	duration: .3,
+// Text
+
+// scroll down 
+let scrollDownBtn = document.getElementById("scroll-down");
+scrollDownBtn?.addEventListener("click", function(){
+  window.scroll(false, banner?.clientHeight)
 });
-tl.from(".lineTwo span", {
-  y: 100,
-  delay: 0,
-	duration: .5,
-  transformOrigin: "0% 50% -50",
-}).to(".lineTwo span", {
-  y: -100,
-  ease: "none",
-  transformOrigin: "0% 50% -50",
-  delay: 1.5,
-	duration: .3,
+
+const swiper = new Swiper(".slider .swiper", {
+  loop: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 // Text
